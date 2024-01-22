c923102465072215025a8cf7eaa41b9988b4871c
OleksiiDmytruk <ljoshadmytruk@gmail.com>
2024-01-22 04:38:33 +0200

    card

--- src/components/CarCard/CarCard.jsx
+++ src/components/CarCard/CarCard.jsx
@@ -9,7 +9,22 @@ export const CarCard = ({ car }) => {
         <p>{car.rentalPrice}</p>
       </div>
       <div>
-        <p></p>
+        <div>
+          <p>{car.address}</p>
+          <div></div>
+          <p>{car.address}</p>
+          <div></div>
+          <p>{car.rentalCompany}</p>
+        </div>
+        <div>
+          <p>{car.type}</p>
+          <div></div>
+          <p>{car.model}</p>
+          <div></div>
+          <p>{car.mileage}</p>
+          <div></div>
+          <p>{car.accessories[0]}</p>
+        </div>
       </div>
       <button>Learn more</button>
     </div>
--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -3,12 +3,15 @@ import { CarCard } from "../CarCard/CarCard";
 export const CarsList = ({ rentalCars }) => {
   console.log(rentalCars);
   return (
-    <ul>
-      {rentalCars.map((car) => (
-        <li key={car.id}>
-          <CarCard car={car} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <ul>
+        {rentalCars.map((car) => (
+          <li key={car.id}>
+            <CarCard car={car} />
+          </li>
+        ))}
+      </ul>
+      <button type="button">Load more</button>
+    </>
   );
 };
