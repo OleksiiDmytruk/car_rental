bbc31eb557f7cf9781f224c1d366e4e0de5a767b
OleksiiDmytruk <ljoshadmytruk@gmail.com>
2024-01-22 23:42:15 +0200

    Updates

---
+++ assets/Home-0VQyIX5J.js
@@ -0,0 +1,36 @@
+import{u as r,j as e}from"./index-M8a-oDPD.js";const n="/car_rental/assets/car-iQjOK_nm.jpg",i=r.section`
+  width: 1280px;
+  height: 600px;
+  position: relative;
+  background: url(${n}) center/cover no-repeat;
+  margin-bottom: 24px;
+`,s=r.h1`
+  position: absolute;
+  top: 10%;
+  left: 10%;
+`,t=r.span`
+  display: block;
+  color: #ff6b0a;
+  text-transform: capitalize;
+  font-size: 60px;
+  font-style: normal;
+  font-weight: 800;
+  line-height: 1.5;
+`,a=r.h2`
+  margin-left: 60px;
+  font-size: 42px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.5;
+  color: #0b44cd;
+  margin-bottom: 24px;
+`,c=r.ul`
+  display: flex;
+  gap: 20px;
+`,o=r.li`
+  width: 240px;
+  border: solid 2px #0b44cd;
+  border-radius: 4px;
+  background: #4c70c25d;
+  padding: 6px;
+`,l=()=>e.jsxs(e.Fragment,{children:[e.jsx(i,{children:e.jsxs(s,{children:[e.jsx(t,{children:" OpenRoad:"}),e.jsx(t,{children:" Your Journey,"}),e.jsx(t,{children:" Your Rules"})]})}),e.jsxs("section",{children:[e.jsx(a,{children:"Here's exactly what we offer you..."}),e.jsxs(c,{children:[e.jsx(o,{children:e.jsx("h3",{children:"Embark on a journey of freedom and flexibility with our premier car rental service! Unleash the thrill of the open road as you choose from our fleet of top-tier vehicles, each meticulously maintained for your comfort and safety."})}),e.jsx(o,{children:e.jsx("h3",{children:"Whether you're exploring the picturesque countryside or navigating the bustling cityscape, our car rental service provides the keys to your personalized adventure. With a seamless booking process and an array of vehicles suited to every taste and occasion, you're not just renting a car – you're embracing a lifestyle of convenience."})}),e.jsx(o,{children:e.jsx("h3",{children:"Experience the joy of the driver's seat and the liberation to chart your own course. From sleek city cruisers to rugged off-road explorers, we've got the wheels to match your style. Revel in the luxury of choice as you embark on a road trip, a weekend escape, or simply elevate your daily commute."})}),e.jsx(o,{children:e.jsx("h3",{children:"Our commitment goes beyond just providing a vehicle; it's about empowering your journey. Enjoy the assurance of 24/7 support, transparent pricing, and a fleet that caters to your preferences. Choose us for your next adventure, and let the road become your canvas."})}),e.jsx(o,{children:e.jsx("h3",{children:"Discover the ease of travel, the thrill of exploration, and the joy of driving with our car rental service. Your journey begins with us – where every mile is a story waiting to be told."})})]})]})]}),d=()=>e.jsx(l,{});export{d as Home,d as default};
